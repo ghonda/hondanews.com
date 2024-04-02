867a9488da4087db26a8f01396ebe2318d68942f
Guilherme Honda <guilhermehonda@gmail.com>
2024-04-02 18:12:39 +0000

    fix database.js

--- infra/database.js
+++ infra/database.js
@@ -1,8 +1,7 @@
 import { Client } from "pg";
 
-let client;
-
 async function query(queryObject) {
+  let client;
   try {
     client = await getNewClient();
     const result = await client.query(queryObject);
