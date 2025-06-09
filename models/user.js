edc8889857bce0a2fb750d0a55be6c4264c9d963
Guilherme Honda <guilhermehonda@gmail.com>
2025-06-09 13:11:44 +0000

    feat: corrige nome do endpoint de usuário e adiciona teste para email duplicado

--- models/user.js
+++ models/user.js
@@ -1,14 +1,20 @@
 import database from "infra/database.js";
 
 async function create(userInputValues) {
-  const results = await database.query({
-    text: "INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING *;",
-    values: [
-      userInputValues.username,
-      userInputValues.email,
-      userInputValues.password,
-    ],
-  });
+  const newUser = await runInsertQuery(userInputValues);
+  return newUser;
+
+  async function runInsertQuery(userInputValues) {
+    const results = await database.query({
+      text: "INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING *;",
+      values: [
+        userInputValues.username,
+        userInputValues.email,
+        userInputValues.password,
+      ],
+    }
+  }
+ );
 
   return results.rows[0];
 }
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
   await orquestrator.runPendingMigrations();
 });
 
-test("POST to /api/v1/annymous user should return 200", async () => {
+test("POST to /api/v1/user should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/users", {
     method: "POST",
     headers: {
@@ -35,4 +35,40 @@ test("POST to /api/v1/annymous user should return 200", async () => {
   expect(uuidVersion(responseBody.id)).toBe(4);
   expect(Date.parse(responseBody.created_at)).not.toBeNaN();
   expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+  
 });
+
+
+test("POST to /api/v1/user duplicate email should return 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "emailduplicado1",
+      email: "duplicado@curso.dev",
+      password: "senha123",
+    }),
+  });
+
+  expect(response1.status).toBe(201);
+  const responseBody = await response1.json();
+
+   const response2 = await fetch("http://localhost:3000/api/v1/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "emailduplicado2",
+      email: "Duplicado@curso.dev",
+      password: "senha123",
+    }),
+   });
+  
+  expect(response2.status).toBe(400);  
+});
+
+
