7b82f669e804255afb4644e48de870036de345a7
Guilherme Honda <guilhermehonda@gmail.com>
2025-10-06 13:42:19 +0000

    feat: refatora autenticação para centralizar lógica de validação de usuário e senha

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,8 +1,6 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
-import user from "models/user.js";
-import password from "models/password.js";
-import { UnauthorizedError } from "infra/errors.js";
+import authentication from "models/authentication.js";
 
 const router = createRouter();
 
@@ -13,24 +11,10 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request, response) {
   const userInputValues = request.body;
 
-  try {
-    const storedUser = await user.findOneByEmail(userInputValues.email);
-    const correctPasswordMatch = await password.compare(
-      userInputValues.password,
-      storedUser.password,
-    );
-    if (!correctPasswordMatch) {
-      throw new UnauthorizedError({
-        message: "Senha não confere",
-        action: "Verifique se o email e a senha estão digitados corretamente",
-      });
-    }
-  } catch (error) {
-    throw new UnauthorizedError({
-      message: "Dados de autenticação inválidos",
-      action: "Verifique se o email e a senha estão digitados corretamente",
-    });
-  }
+  const authenticatedUser = await authentication.getAuthenticatedUser(
+    userInputValues.email,
+    userInputValues.password,
+  );
 
   return response.status(201).json({});
 }
